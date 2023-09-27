bd9e2689dc19824d889d8b06f5806a0547f04c9d
Brian <wamwangimathenge@gmail.com>
2023-09-27 12:55:40 +0300

    Create a script that generates passwords

--- password.sh
+++ password.sh
@@ -4,4 +4,18 @@ echo "Enter the length of the password:"
 
 read -r passLength
 
-echo $passLength
+# echo $passLength
+
+# Define the character set
+character="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+"
+
+password=""
+
+# Loop over the characters
+for ((i=0; i<passLength; i++));
+do
+    random_characters="${character:RANDOM % ${#character}:1}"
+    password="$password$random_characters"
+done
+
+echo "Generated Password: $password"
